498a3e354a740cdde604a7538c4a4b9d526544a2
skholkhojaev <skholkhojaev@adobe.com>
2025-02-24 10:23:32 +0100

    added a test Comment

--- .github/workflows/notify-merge.js
+++ .github/workflows/notify-merge.js
@@ -4,7 +4,7 @@ const SLACK = {
     merge: ({ html_url, number, title, prefix = '' }) =>
         `:merged: PR merged to stage: ${prefix} <${html_url}|#${number}: ${title}>.`,
 };
-// Test
+// Kendrick Better than Drake
 
 function getRepoInfo() {
     const repoFull = process.env.GITHUB_REPOSITORY;
