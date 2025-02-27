2c073b9c493bcef82934b092abf8184d98cecc4b
skholkhojaev <skholkhojaev@adobe.com>
2025-02-27 17:14:32 +0100

    testing notiofuy-merge.js

--- .github/workflows/notify-merge.js
+++ .github/workflows/notify-merge.js
@@ -4,6 +4,7 @@ const SLACK = {
     merge: ({ html_url, number, title, prefix = '' }) =>
         `:merged: PR merged to stage: ${prefix} <${html_url}|#${number}: ${title}>.`,
 };
+// Testtest
 
 const getCommitSha = () => {
     const commitSha = process.env.GITHUB_SHA;
