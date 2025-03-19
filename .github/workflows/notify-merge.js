43e9c8f0b7da2cb0b7c99f7829a738f92dae77d4
Sino Kholkhojaev <132879006+skholkhojaev@users.noreply.github.com>
2025-03-19 09:24:52 +0100

    testing for slack notification (#27)

--- .github/workflows/notify-merge.js
+++ .github/workflows/notify-merge.js
@@ -4,7 +4,7 @@ const SLACK = {
     merge: ({ html_url, number, title, prefix = '' }) =>
         `:merged: PR merged to stage: ${prefix} <${html_url}|#${number}: ${title}>.`,
 };
-// Testtest
+// Testtest SLACK
 
 const getCommitSha = () => {
     const commitSha = process.env.GITHUB_SHA;
