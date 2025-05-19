24b5444a8c2842258e53581ce7a469e1e1e948ec
Sino Kholkhojaev <132879006+skholkhojaev@users.noreply.github.com>
2025-05-19 11:01:18 +0200

    updated the required approvals to test the automated merging

--- .github/workflows/merge-to-stage.js
+++ .github/workflows/merge-to-stage.js
@@ -10,7 +10,7 @@ const {
 // Test For Stage-To-Main
 // Run from the root of the project for local testing: node --env-file=.env .github/workflows/merge-to-stage.js
 const PR_TITLE = '[Release] Stage to Main';
-const REQUIRED_APPROVALS = process.env.REQUIRED_APPROVALS ? Number(process.env.REQUIRED_APPROVALS) : 2;
+const REQUIRED_APPROVALS = process.env.REQUIRED_APPROVALS ? Number(process.env.REQUIRED_APPROVALS) : 0;
 const BASE_MAX_MERGES = process.env.MAX_PRS_PER_BATCH ? Number(process.env.MAX_PRS_PER_BATCH) : 9;
 const MAX_MERGES = BASE_MAX_MERGES + (isWithinPrePostRCP() ? 3 : 0);
 let existingPRCount = 0;
